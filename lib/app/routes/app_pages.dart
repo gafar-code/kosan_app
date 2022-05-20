bd56536a255cc1c8bc6e3a160361f372644bf2db
Gafar <gafartok@gmail.com>
2022-05-20 18:40:02 +0700

    add assets

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 
 import 'package:kosan_app/app/modules/home/bindings/home_binding.dart';
 import 'package:kosan_app/app/modules/home/views/home_view.dart';
+import 'package:kosan_app/app/modules/splash/bindings/splash_binding.dart';
+import 'package:kosan_app/app/modules/splash/views/splash_view.dart';
 
 part 'app_routes.dart';
 
@@ -15,6 +17,11 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => SplashView(),
+      binding: SplashBinding(),
     ),
   ];
 }
